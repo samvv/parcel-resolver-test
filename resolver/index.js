ffb93ab8758483aebbe87a1ee303e17e00394b65
Sam Vervaeck <samvv@pm.me>
2023-04-09 23:11:48 +0200

    Add some comments

--- resolver/index.js
+++ resolver/index.js
@@ -1,14 +1,14 @@
-import {Resolver} from '@parcel/plugin';
+import { Resolver } from '@parcel/plugin';
 import path from 'path';
 
 export default new Resolver({
   async resolve({specifier}) {
     if (specifier === 'special-module') {
-      process.stdout.write(path.join(__dirname) + '\n\n');
-      process.stdout.write(path.join(__dirname, '..', '..', 'example', 'special-modulus.js') + '\n\n');
       return {
+        // This may look arbitrary but I've checked best as Parcel's console allows me to do that this is correct.
         filePath: path.join(__dirname, '..', '..', 'example', 'special-modulus.js'),
-        code: 'import { foo } from "./foo";\nexport default 44;'
+        // This really should be correct. I've even pasted it in TypeScript to be extra sure.
+        code: 'import { foo } from "./foo";\nexport default foo;'
       };
     }
     return null;
